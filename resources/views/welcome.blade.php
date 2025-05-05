8fc743a5ecc5c41f75c8d0643c4f0592712314e7
kimchi <kim24daisuki@gmail.com>
2025-05-05 17:17:49 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+  <h1 class="text-5xl text-red-600 font-bold underline">
+    If you see this big red text, Tailwind works!
+  </h1>
+</body> 
+</html>
